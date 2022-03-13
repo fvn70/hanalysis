34d12a4f8eba938037c70a3d5effd90b1e7777e4
fvn70 <fvn_reg@mail.ru>
2022-03-13 16:15:29 +0700

    Stage 2/5: Merge them

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -1,9 +1,18 @@
 import pandas as pd
 
 pd.set_option('display.max_columns', 8)
+
 df_gen = pd.read_csv('test/general.csv')
 df_pre = pd.read_csv('test/prenatal.csv')
 df_spo = pd.read_csv('test/sports.csv')
-print(df_gen.head(20))
-print(df_pre.head(20))
-print(df_spo.head(20))
+
+# Change the column names
+heads = df_gen.columns
+df_pre.columns = heads
+df_spo.columns = heads
+# Merge the data frames into one.
+df_all = pd.concat([df_gen, df_pre, df_spo], ignore_index=True)
+# Delete the Unnamed: 0 column
+df_all.drop(columns='Unnamed: 0', inplace=True)
+# Print random 20 rows
+print(df_all.sample(n=20, random_state=30))
