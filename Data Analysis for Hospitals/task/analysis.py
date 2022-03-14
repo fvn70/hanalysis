80c69c00a2b5f39b4d7564cdc2e162abdb74f625
fvn70 <fvn_reg@mail.ru>
2022-03-14 23:02:29 +0700

    Stage 3/5: Improve your dataset

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -14,5 +14,16 @@ df_spo.columns = heads
 df_all = pd.concat([df_gen, df_pre, df_spo], ignore_index=True)
 # Delete the Unnamed: 0 column
 df_all.drop(columns='Unnamed: 0', inplace=True)
+# Delete all the empty rows
+df_all.dropna(how='all', inplace=True)
+# Correct all the gender column values to f and m
+df_all.gender = df_all.gender.replace('ma.+', 'm', regex=True)
+df_all.gender = df_all.gender.replace(r'[fw].*', 'f', regex=True)
+# Replace the NaN values in the gender column of the prenatal hospital with f
+df_all.gender = df_all.gender.fillna('f')
+# Replace the NaN values in the bmi ... months columns with zeros
+zeros = {'bmi': 0, 'diagnosis': 0, 'blood_test': 0, 'ecg': 0, 'ultrasound': 0, 'mri': 0, 'xray': 0, 'children': 0, 'months': 0}
+df_all.fillna(value=zeros, inplace=True)
+print(f'Data shape: {df_all.shape}')
 # Print random 20 rows
 print(df_all.sample(n=20, random_state=30))
